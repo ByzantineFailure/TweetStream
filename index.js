9bcbf9ff8c50ed1e3fd27bc4d9af67c128a2127a
ByzantineFailure <byzanitnefailure@gmail.com>
2017-04-21 18:40:09 -0700

    Add an endpoint for something unrelated and stupid

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ var express = require('express'),
     socketApp = require('express-ws')(app),
     heartbeat = require('./lib/heartbeat-pinger'),
     socketHandler = require('./lib/socket-handler'),
+    timeEndpoint = require('./lib/last-time'),
     loggers = require('./lib/logger');
 
 loggers.server.info('Starting server...');
 
 app.use('/', express.static(__dirname + '/build'));
+
+app.get('/time', timeEndpoint.get);
+app.post('/time', timeEndpoint.post);
 
 app.ws('/socket', socketHandler);
 //heartbeat(socketApp.getWss('/socket'));
